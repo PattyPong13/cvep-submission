ce984dc8b18396333acd84d70ac3a4971f134b72
PattyPong13 <90086510+PattyPong13@users.noreply.github.com>
2021-09-04 17:21:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var group_chat = [
     ["Adi", "Ehhhh @Bryan @Patrick how did your H2 math go?"],
+    ["Hilman", "Huh their paper ended alr ah? Morning paper?"],
     ["Player", "Wahhh sian"],
-    ["Player", "Couldn't seem to do all the stats questions"],
+    ["Player", "Couldn't seem to do all the questions"],
+    ["Player", "@Hilman Ya, morning paper,1h paper only. It started at 8am"],
     ["Hilman", "F"],
     ["Hilman", "Bruh I thought yall could use GDC"],
     ["Adi", "Ya bro this paper all the qns can use GDC right?"],
@@ -9,7 +11,10 @@ var group_chat = [
     ["Player", "@Patrick could you do the question on the ducks?"],
     ["Adi", "what ducks"],
     ["Player", "Some stupid question on size of ducks in diff seasons"],
-    ["Adi", "They made us find SD by hand cos they put in some weird algebra"]
+    ["Player", "They made us find SD by hand cos they put in some weird algebra"]
+    ["Hilman", "Wah thank God I took H1 math"],
+    ["Adi", "Wait SD by hand? wth"],
+    ["Hilman", "No lah got some formula for that, very easy one"],
 ];
 
 var patrick_chat = [
